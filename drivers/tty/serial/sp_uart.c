6cfbb49e6aba616eb3a17efdc1582487fb1d46cd
vichsieh <vic.hsieh@sunplus.com>
2018-06-22 14:54:24 +0800

    [UART][DMA] Fix a register setting sequence issue. [Mantis   ID] N/A [Bug    Desc] N/A [Modify Desc] Tx-DMA's txdma_wr_adr must be set before txdma_start_addr [Test   Case] Sending data when UARTn's source clock is 202.5 MHz (not 27 MHz) [Compile] OK [Coverity] N/A [Selftest] OK [GPL-licensed & Third-party Source] N/A

--- drivers/tty/serial/sp_uart.c
+++ drivers/tty/serial/sp_uart.c
@@ -948,8 +948,8 @@ static int sunplus_uart_ops_startup(struct uart_port *port)
 			}
 			DBG_INFO("DMA buffer (Tx) for %s: VA: 0x%p, PA: 0x%x\n", sp_port->name, uartdma_tx->buf_va, (u32)(uartdma_tx->dma_handle));
 
+			writel((u32)(uartdma_tx->dma_handle), &(txdma_reg->txdma_wr_adr));	/* must be set before txdma_start_addr */
 			writel((u32)(uartdma_tx->dma_handle), &(txdma_reg->txdma_start_addr));	/* txdma_reg->txdma_rd_adr is updated by h/w too */
-			writel((u32)(uartdma_tx->dma_handle), &(txdma_reg->txdma_wr_adr));
 			writel(((u32)(uartdma_tx->dma_handle) + UATXDMA_BUF_SZ - 1), &(txdma_reg->txdma_end_addr));
 
 			writel(0x00000005, &(txdma_reg->txdma_enable));		/* Use ring buffer for UART's Tx */
