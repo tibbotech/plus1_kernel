98c91aaca9326c37a4a25f77526fcee174271966
qinjian <qinjian@sunmedia.com.cn>
2018-10-12 12:11:11 +0800

    [PLL] refine dts for PLL [Mantis   ID] N/A [Bug    Desc] N/A [Modify Desc] as title [Test   Case] N/A [Compile] OK [Coverity] N/A [Selftest] OK [GPL-licensed & Third-party Source] N/A

---
+++ include/dt-bindings/clock/sp-q628.h
@@ -0,0 +1,14 @@
+#ifndef _DT_BINDINGS_CLOCK_SUNPLUS_Q628_H
+#define _DT_BINDINGS_CLOCK_SUNPLUS_Q628_H
+
+#define PLL_A        0
+#define PLL_E        1
+#define PLL_E_2P5    2
+#define PLL_E_25     3
+#define PLL_E_112P5  4
+#define PLL_F        5
+#define PLL_TV       6
+#define PLL_TV_A     7
+#define PLL_SYS      8
+
+#endif
