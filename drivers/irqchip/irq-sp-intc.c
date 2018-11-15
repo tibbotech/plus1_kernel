ffae91289483179c79740210c829b77a16fb388f
robert <yusheng.chen@sunplus.com>
2018-11-15 14:05:36 +0800

    [INTCTL][A] Support sp-intc priority ext0/1 selection (SP_INTC_EXT_INT1) [Mantis   ID] N/A [Bug    Desc] N/A [Modify Desc] [Test   Case] Modify irq-sp-intc.c to support EXT_INT1. 	* A EXT_INT0 irq affinity : core0~3 	* A EXT_INT1 irq affinity : core1~3
    	Modify DT to associate a B-side irq to EXT_INT1.
    	For example, assign ehci0 hwirq #14 to EXT_INT1 :
    
    	sp_ehci0: usb@9C102100 {
                interrupts = <14 (IRQ_TYPE_LEVEL_HIGH | SP_INTC_EXT_INT1)>;
    
    	/ # cat /proc/interrupts
    	           CPU0       CPU1       CPU2       CPU3
    	  3:    4656069    4656051    4656044    4656037     GIC-0  29 Level     arch_timer
    	  4:          0          0          0          0     GIC-0  30 Level     arch_timer
    	 16:          0   24171351          0          0   sp_intc  14 Level     ehci_hcd:usb1
    	 53:     620650          0          0          0   sp_intc  53 Level     sp_uart0
    
    [Compile] OK [Coverity] N/A [Selftest] OK
    [GPL-licensed & Third-party Source] N/A
    [GPL-licensed or Third-party Source] N/A
    
    Test ext_int1 with usb0 (copying files):
    	mount -t vfat /dev/sda1 /mnt ;
    	while :; do echo 3 > /proc/sys/vm/drop_caches ; cp -a /mnt/* /tmp/ ; sync ; rm -rf /tmp/*; done &
    	while true; do ramspeed -b 1 >/dev/null; done &
    	while true; do ramspeed -b 1 >/dev/null; done &
    	while true; do ramspeed -b 1 >/dev/null; done &
    	while :; do top -b -n1; cat /proc/interrupts ; sleep 3; done &
    
    For non-B-side GIC irq, just use irq_set_affinity() to assign irq smp affinity. Eg:
    	if (num_online_cpus() > 1) {
    		ret = irq_set_affinity(irq, cpumask_of(1));
    	}

--- drivers/irqchip/irq-sp-intc.c
+++ drivers/irqchip/irq-sp-intc.c
@@ -7,6 +7,11 @@
 #include <linux/irqchip/chained_irq.h>
 #include <linux/of_address.h>
 #include <linux/of_irq.h>
+#include <dt-bindings/interrupt-controller/sp-intc.h>
+
+#if defined(CONFIG_MACH_PENTAGRAM_SC7021_ACHIP) || defined(CONFIG_MACH_PENTAGRAM_SC7021_BCHIP)
+#define SUPPORT_IRQ_GRP_REG
+#endif
 
 #define dprn(...)            /* empty */
 //#define dprn(fmt, arg...)  early_printk(fmt, ##arg)
@@ -29,15 +34,22 @@ struct intG1Reg_st {
 	volatile unsigned int masked_fiqs[7];
 	volatile unsigned int masked_irqs[7];
 	volatile unsigned int g21_reserved_0[10];
+#ifdef SUPPORT_IRQ_GRP_REG
 	volatile unsigned int intr_group;
+#else
+	volatile unsigned int rsvd_31;
+#endif
 };
 
 static struct sp_intctl {
 	__iomem struct intG0Reg_st *g0;
 	__iomem struct intG1Reg_st *g1;
-	 int hwirq_start;
-	 int hwirq_end;   /* exclude */
-	 struct irq_domain *domain;
+	int hwirq_start;
+	int hwirq_end;   /* exclude */
+	struct irq_domain *domain;
+	struct device_node *node;
+	spinlock_t lock;
+	int virq[2];
 } sp_intc;
 
 
@@ -66,7 +78,9 @@ static void sp_intc_ack_irq(struct irq_data *data)
 	idx = data->hwirq / 32;
 	mask = (1 << (data->hwirq % 32));
 
+	spin_lock(&sp_intc.lock);
 	writel(mask, &sp_intc.g1->intr_clr[idx]);
+	spin_unlock(&sp_intc.lock);
 }
 
 static void sp_intc_mask_irq(struct irq_data *data)
@@ -79,9 +93,12 @@ static void sp_intc_mask_irq(struct irq_data *data)
 		return;
 
 	idx = data->hwirq / 32;
+
+	spin_lock(&sp_intc.lock);
 	mask = readl(&sp_intc.g0->intr_mask[idx]);
 	mask &= ~(1 << (data->hwirq % 32));
 	writel(mask, &sp_intc.g0->intr_mask[idx]);
+	spin_unlock(&sp_intc.lock);
 }
 
 static void sp_intc_unmask_irq(struct irq_data *data)
@@ -94,9 +111,11 @@ static void sp_intc_unmask_irq(struct irq_data *data)
 		return;
 
 	idx = data->hwirq / 32;
+	spin_lock(&sp_intc.lock);
 	mask = readl(&sp_intc.g0->intr_mask[idx]);
 	mask |= (1 << (data->hwirq % 32));
 	writel(mask, &sp_intc.g0->intr_mask[idx]);
+	spin_unlock(&sp_intc.lock);
 }
 
 static int sp_intc_set_type(struct irq_data *data, unsigned int flow_type)
@@ -104,6 +123,7 @@ static int sp_intc_set_type(struct irq_data *data, unsigned int flow_type)
 	u32 idx, mask;
 	u32 edge_type; /* 0=level, 1=edge */
 	u32 trig_lvl;  /* 0=high, 1=low */
+	unsigned long flags;
 
 	dprn("%s: hwirq=%lu type=%u\n", __func__, data->hwirq, flow_type);
 
@@ -119,6 +139,9 @@ static int sp_intc_set_type(struct irq_data *data, unsigned int flow_type)
 						   handle_edge_irq, NULL);
 
 	idx = data->hwirq / 32;
+
+	spin_lock_irqsave(&sp_intc.lock, flags);
+
 	edge_type = readl(&sp_intc.g0->intr_type[idx]);
 	trig_lvl = readl(&sp_intc.g0->intr_polarity[idx]);
 	mask = (1 << (data->hwirq % 32));
@@ -141,21 +164,45 @@ static int sp_intc_set_type(struct irq_data *data, unsigned int flow_type)
 		writel((trig_lvl | mask), &sp_intc.g0->intr_polarity[idx]);
 		break;
 	default:
+		spin_unlock_irqrestore(&sp_intc.lock, flags);
 		pr_err("%s: type=%d\n", __func__, flow_type);
 		return -EBADR;
 	}
 
+	spin_unlock_irqrestore(&sp_intc.lock, flags);
+
 	return IRQ_SET_MASK_OK;
 }
 
-/* return -1 if no interrupt */
-static int sp_intc_get_hwirq(void)
+/* prio=1 (normal), prio=0 (dedicated) */
+static void sp_intc_set_prio(u32 hwirq, u32 prio)
+{
+	u32 idx, mask;
+
+	pr_info("set hwirq=%u prio=%u\n", hwirq, prio);
+
+	if ((hwirq < sp_intc.hwirq_start) || (hwirq >= sp_intc.hwirq_end))
+		return;
+
+	idx = hwirq / 32;
+
+	spin_lock(&sp_intc.lock);
+	mask = readl(&sp_intc.g0->priority[idx]);
+	if (prio)
+		mask |= (1 << (hwirq % 32));
+	else
+		mask &= ~(1 << (hwirq % 32));
+	writel(mask, &sp_intc.g0->priority[idx]);
+	spin_unlock(&sp_intc.lock);
+}
+
+/* return -1 if no interrupt # */
+static int sp_intc_get_ext0_irq(void)
 {
 	int hwirq, mask;
 	int i;
 
-	/* look up reg number i */
-#if defined(CONFIG_MACH_PENTAGRAM_SC7021_ACHIP) || defined(CONFIG_MACH_PENTAGRAM_SC7021_BCHIP)
+#ifdef SUPPORT_IRQ_GRP_REG
 	mask = (readl(&sp_intc.g1->intr_group) >> 8) & 0x7f; /* [14:8] for irq group */
 	if (mask) {
 		i = fls(mask) - 1;
@@ -165,21 +212,57 @@ static int sp_intc_get_hwirq(void)
 		mask = readl(&sp_intc.g1->masked_irqs[i]);
 		if (mask) {
 			hwirq = (i << 5) + fls(mask) - 1;
-			dprn(" [%d]", hwirq);
+			dprn(" I[%d]", hwirq);
 			return hwirq;
 		}
 	}
 	return -1;
 }
 
-static void sp_intc_handle_irq_cascaded(struct irq_desc *desc)
+static int sp_intc_get_ext1_irq(void)
+{
+	int hwirq, mask;
+	int i;
+
+#ifdef SUPPORT_IRQ_GRP_REG
+	mask = (readl(&sp_intc.g1->intr_group) >> 0) & 0x7f; /* [6:0] for fiq group */
+	if (mask) {
+		i = fls(mask) - 1;
+#else
+	for (i = 0; i < 7; i++) {
+#endif
+		mask = readl(&sp_intc.g1->masked_fiqs[i]);
+		if (mask) {
+			hwirq = (i << 5) + fls(mask) - 1;
+			dprn(" F[%d]", hwirq);
+			return hwirq;
+		}
+	}
+	return -1;
+}
+
+static void sp_intc_handle_ext0_cascaded(struct irq_desc *desc)
 {
         struct irq_chip *host_chip = irq_desc_get_chip(desc);
 	int hwirq;
 
         chained_irq_enter(host_chip, desc);
 
-	while ((hwirq = sp_intc_get_hwirq()) >= 0) {
+	while ((hwirq = sp_intc_get_ext0_irq()) >= 0) {
+                generic_handle_irq(irq_find_mapping(sp_intc.domain, (unsigned int)hwirq));
+        }
+
+        chained_irq_exit(host_chip, desc);
+}
+
+static void sp_intc_handle_ext1_cascaded(struct irq_desc *desc)
+{
+        struct irq_chip *host_chip = irq_desc_get_chip(desc);
+	int hwirq;
+
+        chained_irq_enter(host_chip, desc);
+
+	while ((hwirq = sp_intc_get_ext1_irq()) >= 0) {
                 generic_handle_irq(irq_find_mapping(sp_intc.domain, (unsigned int)hwirq));
         }
 
@@ -191,7 +274,7 @@ static int sp_intc_handle_one_round(struct pt_regs *regs)
 	int ret = -EINVAL;
 	int hwirq;
 
-	while ((hwirq = sp_intc_get_hwirq()) >= 0) {
+	while ((hwirq = sp_intc_get_ext0_irq()) >= 0) {
 		handle_domain_irq(sp_intc.domain, hwirq, regs);
 		ret = 0;
 	}
@@ -259,8 +342,32 @@ static void __init sp_intc_chip_init(int hwirq_start, int hwirq_end,
 	}
 }
 
+int sp_intc_xlate_of(struct irq_domain *d, struct device_node *node,
+			  const u32 *intspec, unsigned int intsize,
+			  irq_hw_number_t *out_hwirq, unsigned int *out_type)
+{
+	int ret = 0;
+	u32 ext_num;
+
+	if (WARN_ON(intsize < 2))
+		return -EINVAL;
+
+	*out_hwirq = intspec[0];
+	*out_type = intspec[1] & IRQ_TYPE_SENSE_MASK;
+	ext_num = (intspec[1] & SP_INTC_EXT_INT_MASK) >> SP_INTC_EXT_INT_SHFIT;
+
+	/* set ext_int1 to high priority */
+	if (ext_num != 1) {
+		ext_num = 0;
+	}
+	if (ext_num) {
+		sp_intc_set_prio(*out_hwirq, 0); /* priority=0 */
+	}
+	return ret;
+}
+
 static struct irq_domain_ops sp_intc_dm_ops = {
-	.xlate = irq_domain_xlate_twocell,
+	.xlate = sp_intc_xlate_of,
 	.map = sp_intc_irq_domain_map,
 };
 
@@ -280,11 +387,58 @@ int __init sp_intc_init(int hwirq_start, int irqs, void __iomem *base0, void __i
 	return 0;
 }
 
+#ifdef CONFIG_MACH_PENTAGRAM_SC7021_ACHIP
+static cpumask_t *u2cpumask(u32 mask, cpumask_t *cpumask)
+{
+	unsigned int i;
+	for (i = 0; i < 4; i++) {
+		if (mask & (1 << i))
+			cpumask_set_cpu(i, cpumask);
+		else
+			cpumask_clear_cpu(i, cpumask);
+	}
+	return cpumask;
+}
+
+static int __init sp_intc_ext_adjust(void)
+{
+	u32 mask;
+	static cpumask_t cpumask;
+	struct device_node *node = sp_intc.node;
+
+	dprn("%s\n", __func__);
+
+	if (num_online_cpus() <= 1) {
+		pr_info("single core: skip ext adjust\n");
+		return 0;
+	}
+
+	cpumask = CPU_MASK_NONE;
+	if (!of_property_read_u32(node, "ext0-mask", &mask)) {
+		printk("%d: ext0-mask=0x%x\n", sp_intc.virq[0], mask);
+		if (irq_set_affinity(sp_intc.virq[0], u2cpumask(mask, &cpumask))) {
+			pr_err("failed to set ext0 cpumask=0x%x\n", mask);
+		}
+	}
+
+	cpumask = CPU_MASK_NONE;
+	if (!of_property_read_u32(node, "ext1-mask", &mask)) {
+		printk("%d: ext1-mask=0x%x\n", sp_intc.virq[1], mask);
+		if (irq_set_affinity(sp_intc.virq[1], u2cpumask(mask, &cpumask))) {
+			pr_err("failed to set ext1 cpumask=0x%x\n", mask);
+		}
+	}
+
+	return 0;
+}
+arch_initcall(sp_intc_ext_adjust)
+#endif
+
 #ifdef CONFIG_OF
+
 int __init sp_intc_init_dt(struct device_node *node, struct device_node *parent)
 {
 	void __iomem *base0, *base1;
-	int irq;
 
 	dprn("%s\n", __func__);
 
@@ -296,6 +450,8 @@ int __init sp_intc_init_dt(struct device_node *node, struct device_node *parent)
 	if (!base1)
 		panic("unable to map sp-intc base 1\n");
 
+	sp_intc.node = node;
+
 	sp_intc_chip_init(SP_INTC_HWIRQ_MIN, SP_INTC_HWIRQ_MAX, base0, base1);
 
 	sp_intc.domain = irq_domain_add_linear(node, sp_intc.hwirq_end - sp_intc.hwirq_start,
@@ -303,13 +459,23 @@ int __init sp_intc_init_dt(struct device_node *node, struct device_node *parent)
 	if (!sp_intc.domain)
 		panic("%s: unable to create linear domain\n", __func__);
 
+	spin_lock_init(&sp_intc.lock);
+
         if (parent) {
-		irq = irq_of_parse_and_map(node, 0);
-		if (irq) {
-			irq_set_chained_handler_and_data(irq,
-				sp_intc_handle_irq_cascaded, &sp_intc);
+		sp_intc.virq[0] = irq_of_parse_and_map(node, 0);
+		if (sp_intc.virq[0]) {
+			irq_set_chained_handler_and_data(sp_intc.virq[0],
+				sp_intc_handle_ext0_cascaded, &sp_intc);
 		} else {
-			panic("%s: missed irq in DT\n", __func__);
+			panic("%s: missed ext0 irq in DT\n", __func__);
+		}
+
+		sp_intc.virq[1] = irq_of_parse_and_map(node, 1);
+		if (sp_intc.virq[1]) {
+			irq_set_chained_handler_and_data(sp_intc.virq[1],
+				sp_intc_handle_ext1_cascaded, &sp_intc);
+		} else {
+			panic("%s: missed ext1 irq in DT\n", __func__);
 		}
 	} else {
 		set_handle_irq(sp_intc_handle_irq);
