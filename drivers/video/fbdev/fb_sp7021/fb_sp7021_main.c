6b9a93d9e5450788b9b6c213e1cfee73ece775fb
pochou.chen <pochou.chen@sunplus.com>
2019-03-19 15:07:21 +0800

    [FB] fix coding style. [Mantis ID] N/A [Bug  Desc] N/A [Modify Desc] [Test Case] N/A [Compile] OK [Coverity] N/A [Selftest] OK [GPL-Licensed & Third-party Source] N/A

--- drivers/video/fbdev/fb_sp7021/fb_sp7021_main.c
+++ drivers/video/fbdev/fb_sp7021/fb_sp7021_main.c
@@ -178,8 +178,8 @@ static int _sp7021_fb_create_device(struct platform_device *pdev,
 	fbWorkMem->fbsize = fbWorkMem->fbpagesize * fbWorkMem->fbpagenum;
 
 	/* Allocate Buffer */
-	fbWorkMem->fbmem = (void __iomem *)dma_zalloc_coherent(&pdev->dev, fbWorkMem->fbsize,
-			&fb_phymem, GFP_KERNEL);
+	fbWorkMem->fbmem = (void __iomem *)dma_zalloc_coherent(&pdev->dev,
+			fbWorkMem->fbsize, &fb_phymem, GFP_KERNEL);
 
 	if (!fbWorkMem->fbmem) {
 		mod_err(pdev, "malloc failed, size %d(%dx%dx(%d/8)*%d)\n",
