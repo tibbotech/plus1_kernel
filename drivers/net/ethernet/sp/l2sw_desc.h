203af70f05382c7e398e3142fefe806e5668f72e
Wells Lu <wells.lu@sunplus.com>
2019-04-12 17:47:00 +0800

    [drivers][net][ethernet][sp] Changed name of some variables (give proper name). [Mantis ID] N/A [Bug Desc] N/A [Modify Desc] As titled [Test   Case] N/A [Compile] OK [Coverity] N/A [Selftest] OK [GPL-licensed & Third-party Source] N/A

--- drivers/net/ethernet/sp/l2sw_desc.h
+++ drivers/net/ethernet/sp/l2sw_desc.h
@@ -4,23 +4,23 @@
 #include "l2sw_define.h"
 
 
-void rx_descs_flush(struct l2sw_common *mac);
+void rx_descs_flush(struct l2sw_common *comm);
 
-void tx_descs_clean(struct l2sw_common *mac);
+void tx_descs_clean(struct l2sw_common *comm);
 
-void rx_descs_clean(struct l2sw_common *mac);
+void rx_descs_clean(struct l2sw_common *comm);
 
-void descs_clean(struct l2sw_common *mac);
+void descs_clean(struct l2sw_common *comm);
 
-void descs_free(struct l2sw_common *mac);
+void descs_free(struct l2sw_common *comm);
 
-u32 tx_descs_init(struct l2sw_common *mac);
+u32 tx_descs_init(struct l2sw_common *comm);
 
-u32 rx_descs_init(struct l2sw_common *mac);
+u32 rx_descs_init(struct l2sw_common *comm);
 
-u32 descs_alloc(struct l2sw_common *mac);
+u32 descs_alloc(struct l2sw_common *comm);
 
-u32 descs_init(struct l2sw_common *mac);
+u32 descs_init(struct l2sw_common *comm);
 
 
 #endif
