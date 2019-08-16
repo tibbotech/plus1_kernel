1c43791bbded96c4f4c1ac8b0617cfe2a82710df
cc.ho <cc.ho@sunplus.com>
2019-08-16 15:43:18 +0800

    [drivers][dma][sp-cbdma] Develop User Space DMA [Mantis   ID] N/A [Bug    Desc] N/A [Modify Desc] Develop user space DMA to copy video data for mipi. (complete but illegal) [Test   Case] Transfer raw data from frame buffer to dma proxy channel interface buffer [Compile] OK [Coverity] N/A [Selftest] OK [GPL-licensed & Third-party Source] N/A

--- drivers/dma/sp-cbdma/sp_cbdma_proxy.h
+++ drivers/dma/sp-cbdma/sp_cbdma_proxy.h
@@ -12,5 +12,6 @@ struct dma_proxy_channel_interface {
 	unsigned char buffer[TEST_SIZE];
 	enum proxy_status { PROXY_NO_ERROR = 0, PROXY_BUSY = 1, PROXY_TIMEOUT = 2, PROXY_ERROR = 3 } status;
 	unsigned int length;
+	void *src;
 };
 
