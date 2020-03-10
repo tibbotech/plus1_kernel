5c56b67c9e3004843a91d883ab87fc4e786efa09
vincent.shih <vincent.shih@sunext.com>
2020-03-10 19:13:11 +0800

    [drivers][USB]Add the USB host controller drivers               of ohci and ehci. [Mantis ID] N/A [Bug Desc] N/A [Modify Desc] As titled. [Test Case] N/A [Compile] Ok [Coverity] N/A [Selftest] Ok [GPL-licensed & Third-party Source] N/A

---
+++ drivers/usb/host/ohci1-sunplus.c
@@ -0,0 +1,67 @@
+#include <linux/usb/sp_usb.h>
+#include <linux/module.h>
+#include <linux/platform_device.h>
+#include "ohci-sunplus.h"
+
+#if 0
+extern u8 sp_port1_enabled;
+#endif
+
+extern void usb_hcd_platform_shutdown(struct platform_device *dev);
+
+static int ohci1_sunplus_platform_probe(struct platform_device *dev){
+
+	dev->id = 2;
+
+	return ohci_sunplus_probe(dev);
+}
+
+
+static const struct of_device_id ohci1_sunplus_dt_ids[] = {
+	{ .compatible = "sunplus,sunplus-i143-usb-ohci1" },
+	{ }
+};
+
+MODULE_DEVICE_TABLE(of, ohci1_sunplus_dt_ids);
+
+static struct platform_driver ohci1_hcd_sunplus_driver = {
+	.probe			= ohci1_sunplus_platform_probe,
+	.remove			= ohci_sunplus_remove,
+	.shutdown		= usb_hcd_platform_shutdown,
+	.driver = {
+		.name		= "ohci1-sunplus",
+		.of_match_table = ohci1_sunplus_dt_ids,
+#ifdef CONFIG_PM
+		.pm = &ohci_sunplus_pm_ops,
+#endif
+	}
+};
+
+/*-------------------------------------------------------------------------*/
+
+static int __init ohci1_sunplus_init(void)
+{
+#if 0
+	if (sp_port1_enabled & PORT1_ENABLED){
+		printk(KERN_NOTICE "register ohci1_hcd_sunplus_driver\n");
+		return platform_driver_register(&ohci1_hcd_sunplus_driver);	
+	} else {
+		printk(KERN_NOTICE "warn,port1 not enable,not register ohci1 sunplus hcd driver\n");
+		return -1;
+	}
+#else
+	//printk(KERN_NOTICE "register ohci1_hcd_sunplus_driver\n");
+	//return platform_driver_register(&ohci1_hcd_sunplus_driver);
+#endif
+}
+module_init(ohci1_sunplus_init);
+
+static void __exit ohci1_sunplus_cleanup(void)
+{
+	platform_driver_unregister(&ohci1_hcd_sunplus_driver);
+}
+module_exit(ohci1_sunplus_cleanup);
+
+MODULE_ALIAS("platform:ohci1-sunplus");
+MODULE_LICENSE("GPL");
+
