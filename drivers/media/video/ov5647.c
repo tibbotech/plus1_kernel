c5cdd85a3c58a37b3409da0b2cf9780b779a52c4
Wells Lu <wells.lu@sunplus.com>
2020-05-20 14:19:35 +0800

    [drivers][media][video] Use macro 'module_i2c_driver' to declare module. [Mantis ID] N/A [Bug Desc] N/A [Modify Desc] As titled. [Test Case] N/A [Compile] OK [Coverity] N/A [Selftest] OK [GPL-licensed & Third-party Source] N/A

--- drivers/media/video/ov5647.c
+++ drivers/media/video/ov5647.c
@@ -647,19 +647,7 @@ static struct i2c_driver ov5647_i2c_driver = {
 	},
 };
 
-
-static int __init sensor_mod_init(void)
-{
-	return i2c_add_driver(&ov5647_i2c_driver);
-}
-
-static void __exit sensor_mod_exit(void)
-{
-	i2c_del_driver(&ov5647_i2c_driver);
-}
-
-module_init(sensor_mod_init);
-module_exit(sensor_mod_exit);
+module_i2c_driver(ov5647_i2c_driver);
 
 MODULE_DESCRIPTION("Sunplus ov5647 sensor driver");
 MODULE_LICENSE("GPL v2");
