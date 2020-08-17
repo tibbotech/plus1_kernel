f3a6be47dd9362d728af5174f2340f142de219c6
xt.hu <xt.hu@sunmedia.com.cn>
2020-08-17 14:59:02 +0800

    [drivers][gpu_2d] fix a Macro

--- drivers/gpu/gpu_2d/hal/os/linux/kernel/gc_hal_kernel_probe.c
+++ drivers/gpu/gpu_2d/hal/os/linux/kernel/gc_hal_kernel_probe.c
@@ -173,7 +173,7 @@ module_param(gpuProfiler, int, 0644);
 static int signal = 48;
 module_param(signal, int, 0644);
 
-#if 1
+#ifdef CONFIG_RISCV
 static ulong baseAddress = 0xA0000000;
 #else
 static ulong baseAddress = 0x20000000;
