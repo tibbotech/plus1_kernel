fb08d5aef7a989e4d147ac06b85144e11b66c72a
Wells Lu <wells.lu@sunplus.com>
2021-04-22 17:57:00 +0800

    [Q645] Updated pin-ctrl settings.

--- drivers/pinctrl/sunplus/pinctrl_inf_q645.c
+++ drivers/pinctrl/sunplus/pinctrl_inf_q645.c
@@ -53,9 +53,11 @@ const char * const sppctlpmux_list_s[] = {
 // gpio: is defined in gpio_inf_q645.c
 const size_t PMUX_listSZ = sizeof(sppctlpmux_list_s)/sizeof(*(sppctlpmux_list_s));
 
-static const unsigned pins_spif[] = { 6, 7, 8, 9, 10, 11 };
+static const unsigned pins_spif1[] = {  6,  7,  8 , 9, 10, 11 };
+static const unsigned pins_spif2[] = { 22, 23, 24, 25, 26, 27 };
 static const sppctlgrp_t q645grps_spif[] = {
-	EGRP("SPI_FLASH", 1, pins_spif)
+	EGRP("SPI_FLASH1", 1, pins_spif1),
+	EGRP("SPI_FLASH2", 2, pins_spif2)
 };
 
 static const unsigned pins_emmc[] = { 12, 13, 14, 15, 16, 17, 18, 19, 20, 21 };
@@ -63,9 +65,11 @@ static const sppctlgrp_t q645grps_emmc[] = {
 	EGRP("CARD0_EMMC", 1, pins_emmc)
 };
 
-static const unsigned pins_snand[] = { 22, 23, 24, 25, 26, 27 };
+static const unsigned pins_snand1[] = { 22, 23, 24, 25, 26, 27 };
+static const unsigned pins_snand2[] = { 6,   7,  8,  9, 10, 11 };
 static const sppctlgrp_t q645grps_snand[] = {
-	EGRP("SPI_NAND", 1, pins_snand)
+	EGRP("SPI_NAND1", 1, pins_snand1),
+	EGRP("SPI_NAND2", 2, pins_snand2)
 };
 
 static const unsigned pins_sdc30[] = { 28, 29, 30, 31, 32, 33 };
@@ -98,14 +102,9 @@ static const sppctlgrp_t q645grps_uart3[] = {
 	EGRP("UART3", 1, pins_uart3)
 };
 
-static const unsigned pins_spim0[] = { 56, 57, 58, 59, 60 };
-static const sppctlgrp_t q645grps_spim0[] = {
-	EGRP("SPI_MASTER0", 1, pins_spim0)
-};
-
-static const unsigned pins_spis0[] = { 56, 57, 58, 59, 60 };
-static const sppctlgrp_t q645grps_spis0[] = {
-	EGRP("SPI_SLAVE0", 1, pins_spis0)
+static const unsigned pins_spicombo0[] = { 12, 13, 14, 15, 18, 19, 20, 21 };
+static const sppctlgrp_t q645grps_spicombo0[] = {
+	EGRP("SPI_COMBO0", 1, pins_spicombo0)
 };
 
 static const unsigned pins_u2otg[] = { 61 };
@@ -113,7 +112,7 @@ static const sppctlgrp_t q645grps_u2otg[] = {
 	EGRP("USB2_OTG", 1, pins_u2otg)
 };
 
-static const unsigned pins_i2cm0[] = { 64, 65 };
+static const unsigned pins_i2cm0[] = { 59, 60 };
 static const sppctlgrp_t q645grps_i2cm0[] = {
 	EGRP("I2C_MASTER0", 1, pins_i2cm0)
 };
@@ -128,8 +127,8 @@ static const sppctlgrp_t q645grps_wakeup1[] = {
 	EGRP("WAKEUP1", 1, pins_wakeup1)
 };
 
-static const unsigned pins_int_x1[] = { 77 };
-static const unsigned pins_int_x2[] = { 78 };
+static const unsigned pins_int_x1[] = { 78 };
+static const unsigned pins_int_x2[] = { 79 };
 static const unsigned pins_int_x3[] = { 52 };
 static const unsigned pins_int_x4[] = { 53 };
 static const unsigned pins_int_x5[] = { 49 };
@@ -212,29 +211,28 @@ func_t list_funcs[] = {
 	FNCN("GPIO",            fOFF_0, 0, 0, 0),
 	FNCN("IOP",             fOFF_0, 0, 0, 0),
 
-	FNCE("SPI_FLASH",       fOFF_G, 1, 0, 1, q645grps_spif),
-	FNCE("CARD0_EMMC",      fOFF_G, 1, 1, 1, q645grps_emmc),
-	FNCE("SPI_NAND",        fOFF_G, 1, 2, 1, q645grps_snand),
-	FNCE("SD_CARD",         fOFF_G, 1, 3, 1, q645grps_sdc30),
-	FNCE("SDIO",            fOFF_G, 1, 4, 1, q645grps_sdio30),
-	FNCE("UART0",           fOFF_G, 1, 5, 1, q645grps_uart0),
-	FNCE("UART1",           fOFF_G, 1, 6, 1, q645grps_uart1),
-	FNCE("UART2",           fOFF_G, 1, 7, 1, q645grps_uart2),
-	FNCE("UART3",           fOFF_G, 1, 8, 1, q645grps_uart3),
+	FNCE("SPI_FLASH",       fOFF_G, 1, 0, 2, q645grps_spif),
+	FNCE("CARD0_EMMC",      fOFF_G, 1, 2, 1, q645grps_emmc),
+	FNCE("SPI_NAND",        fOFF_G, 1, 3, 2, q645grps_snand),
+	FNCE("SD_CARD",         fOFF_G, 1, 5, 1, q645grps_sdc30),
+	FNCE("SDIO",            fOFF_G, 1, 6, 1, q645grps_sdio30),
+	FNCE("UART0",           fOFF_G, 1, 7, 1, q645grps_uart0),
+	FNCE("UART1",           fOFF_G, 1, 8, 1, q645grps_uart1),
+	FNCE("UART2",           fOFF_G, 1, 9, 1, q645grps_uart2),
+	FNCE("UART3",           fOFF_G, 1,10, 1, q645grps_uart3),
+	FNCE("SPI_COMBO0",      fOFF_G, 1,12, 1, q645grps_spicombo0),
+	FNCE("I2C_MASTER0",     fOFF_G, 1,13, 1, q645grps_i2cm0),
 
-	FNCE("INT0",            fOFF_G, 2, 0, 3, q645grps_int0),
-	FNCE("INT1",            fOFF_G, 2, 3, 3, q645grps_int1),
-	FNCE("INT2",            fOFF_G, 2, 6, 3, q645grps_int2),
-	FNCE("INT3",            fOFF_G, 2, 9, 3, q645grps_int3),
-	FNCE("INT4",            fOFF_G, 2,12, 3, q645grps_int4),
-	FNCE("INT5",            fOFF_G, 3, 0, 3, q645grps_int5),
-	FNCE("INT6",            fOFF_G, 3, 3, 3, q645grps_int6),
-	FNCE("INT7",            fOFF_G, 3, 6, 3, q645grps_int7),
+	FNCE("INT0",            fOFF_G, 2, 2, 3, q645grps_int0),
+	FNCE("INT1",            fOFF_G, 2, 5, 3, q645grps_int1),
+	FNCE("INT2",            fOFF_G, 2, 8, 3, q645grps_int2),
+	FNCE("INT3",            fOFF_G, 2,11, 3, q645grps_int3),
+	FNCE("INT4",            fOFF_G, 3, 0, 3, q645grps_int4),
+	FNCE("INT5",            fOFF_G, 3, 3, 3, q645grps_int5),
+	FNCE("INT6",            fOFF_G, 3, 6, 3, q645grps_int6),
+	FNCE("INT7",            fOFF_G, 3, 7, 3, q645grps_int7),
 
-	FNCE("SPI_MASTER0",     fOFF_G, 2,15, 1, q645grps_spim0),  // TO BE CONFIRMED
-	FNCE("SPI_SLAVE0",      fOFF_G, 2,15, 1, q645grps_spis0),  // TO BE CONFIRMED
-	FNCE("USB2_OTG",        fOFF_G, 2,15, 1, q645grps_u2otg),  // TO BE CONFIRMED
-	FNCE("I2C_MASTER0",     fOFF_G, 2,15, 1, q645grps_i2cm0),  // TO BE CONFIRMED
+	FNCE("USB2_OTG",        fOFF_G, 2,15, 1, q645grps_u2otg),    // TO BE CONFIRMED
 	FNCE("WAKEUP0",         fOFF_G, 2,15, 1, q645grps_wakeup0),  // TO BE CONFIRMED
 	FNCE("WAKEUP1",         fOFF_G, 2,15, 1, q645grps_wakeup1)   // TO BE CONFIRMED
 };
