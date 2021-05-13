e6708f6eaa172876236b253212443e898792af76
tony.huang <tony.huang@sunplus.com>
2021-05-13 16:57:36 +0800

    [Q645]Added IOP device driver.

--- drivers/misc/iop/sp_iop.h
+++ drivers/misc/iop/sp_iop.h
@@ -1,6 +1,8 @@
 #ifndef __SP_IOP_H__
 #define __SP_IOP_H__
+#ifdef CONFIG_SOC_SP7021
 #include <mach/io_map.h>
+#endif 
 #include "hal_iop.h"
 typedef enum IOP_Status_e_ {
 	IOP_SUCCESS,                /* successful */
