62f7f731ab2e4be6820a73a607ca5e2e701f255f
qinjian <qinjian@cqplus1.com>
2021-07-19 10:45:26 +0800

    [drivers][tty] rename sunplus uart device

--- drivers/tty/serial/sp_uart.c
+++ drivers/tty/serial/sp_uart.c
@@ -61,7 +61,7 @@
 #define DBG_ERR(fmt, args ...)
 #endif
 /* ---------------------------------------------------------------------------------------------- */
-#define DEVICE_NAME			"ttyS"
+#define DEVICE_NAME			"ttyPS"
 #define SP_UART_MAJOR			TTY_MAJOR
 #define SP_UART_MINOR_START		64
 
