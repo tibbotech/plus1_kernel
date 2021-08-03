3fad8368cfc6ce58f83b4fc96b6086c9ce8aca4b
tony.huang <tony.huang@sunplus.com>
2021-08-03 13:48:10 +0800

    [Q645][kernel][power] Define arm_pm_restart for reboot command.

--- drivers/power/reset/sp-poweroff.c
+++ drivers/power/reset/sp-poweroff.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- * 
+ *
  */
 #include <linux/kernel.h>
 #include <linux/init.h>
@@ -12,6 +12,10 @@
 #ifdef CONFIG_SUNPLUS_IOP
 #include <../drivers/misc/iop/sp_iop.h>
 #endif
+#include <linux/reboot.h>
+#include <linux/io.h>
+#include <asm/system_misc.h>
+
 
 static void sp_power_off(void)
 {
@@ -22,11 +26,37 @@ static void sp_power_off(void)
 	while (1);
 }
 
+void sp_restart(enum reboot_mode mode, const char *cmd)
+{
+	void __iomem *regs = NULL;
+
+	#ifdef CONFIG_SUNPLUS_IOP
+	unsigned int reg_value;
+	#endif
+	early_printk("%s\n", __func__);
+	regs = ioremap(0xF8000000, 0x32);
+	/* MOON : enable watchdog reset */
+	writel(0x06000600, (regs + 0x0248));
+
+	/* STC: watchdog control */
+	writel(0x3877, (regs + 0x0628)); /* stop */
+	writel(0xAB00, (regs + 0x0628)); /* unlock */
+	writel(0x0001, (regs + 0x062C)); /* counter */
+	writel(0x4A4B, (regs + 0x0628)); /* resume */
+
+	#ifdef CONFIG_SUNPLUS_IOP
+	reg_value = readl((regs + 0x400));
+	writel((reg_value|0x0001), (regs + 0x400));
+	#endif
+}
+
+
 
 static int sp_poweroff_probe(struct platform_device *pdev)
 {
 	//pr_err("sp_poweroff_probe\n");
 	pm_power_off = sp_power_off;
+	arm_pm_restart = sp_restart;
 	return 0;
 }
 
