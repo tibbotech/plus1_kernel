228e611071d3071204c69fc98a2b03e143c00bc3
hammer.hsieh <hammer.hsieh@sunplus.com>
2021-09-14 14:09:02 +0800

    [drivers][misc] Modify sunplus icm(input capture module) driver for linux coding style.

--- drivers/misc/sp_icm.c
+++ drivers/misc/sp_icm.c
@@ -1,3 +1,8 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Sunplus SP7021 SoC ICM(Input Capture Module) driver
+ */
+
 #include <linux/module.h>
 #include <linux/types.h>
 #include <linux/io.h>
@@ -26,18 +31,22 @@ do { \
 	pm_runtime_put(dev); \
 } while (0)
 
-// cfg0
-#define ICM_ENABLE		0x00010001	// enable icm
-#define ICM_DISABLE		0x00010000	// disable icm
-#define ICM_RELOAD		0x00020002	// reload icm settings
-#define ICM_INTCLR		0x00040004	// clear icm interrupt
+/*
+ * cfg0
+ */
+#define ICM_ENABLE		0x00010001	/* enable icm */
+#define ICM_DISABLE		0x00010000	/* disable icm */
+#define ICM_RELOAD		0x00020002	/* reload icm settings */
+#define ICM_INTCLR		0x00040004	/* clear icm interrupt */
 
 #define MUXSEL_OFS	3
 #define MUXSEL_BITS	3
 #define CLKSEL_OFS	6
 #define CLKSEL_BITS	3
 
-// cfg1
+/*
+ * cfg1
+ */
 #define EEMODE_OFS	0
 #define EEMODE_BITS	2
 #define ETIMES_OFS	2
@@ -45,53 +54,61 @@ do { \
 #define DTIMES_OFS	6
 #define DTIMES_BITS	3
 
-#define ICM_FCLEAR	0x20002000	// clear fifo, also fddrop=0, fempty=1, ffull=0
+#define ICM_FCLEAR	0x20002000
 #define ICM_FMASK	(ICM_FDDROP | ICM_FEMPTY | ICM_FFULL)
 
 #define ICM_MSK(field)  (((1 << field##_BITS) - 1) << field##_OFS)
 
 #define ICM_SETCFG(_cfg, field, val) \
-	do { \
-		icm->cfg##_cfg = ((val) << field##_OFS) | (ICM_MSK(field) << 16); \
-	} while (0)
+	(icm->cfg##_cfg = (((val) << field##_OFS) | (ICM_MSK(field) << 16)))
+
 #define ICM_GETCFG(_cfg, field) \
 	(((icm->cfg##_cfg) & ICM_MSK(field)) >> field##_OFS)
 
+/*
+ * cfg0
+ * u32 enable:1;
+ * u32 reload:1; reload setting
+ * u32 intclr:1; write 1 clear interrupt
+ * u32 muxsel:3; select input signal source
+ * u32 clksel:3; select clock source for counter
+ * u32 rsv0:7;
+ * u32 msk0:16;
+ *
+ * cfg1
+ * u32 eemode:2; edge mode: 0 rising / 1 falling / 2 both
+ * u32 etimes:4; event times (0~15)
+ * u32 dtimes:3; debounce times (0~7)
+ * u32 rsv1:3;
+ * u32 fddrop:1; fifo data drop
+ * u32 fclear:1; fifo clear
+ * u32 fempty:1; fifo empty
+ * u32 ffull :1; fifo full
+ * u32 msk1:16;
+ *
+ * u32 cntscl; counter clock prescaler:
+ *     cnt_clk = ext_clk / (cnt_scl + 1)
+ * u32 tstscl; test signal clock prescaler:
+ *     tst_clk = sysclk / (tst_scl + 1)
+ * u32 cnt; counter, read from fifo
+ *
+ * u32 pwh; pulse width high
+ * u32 pwl; pulse width low
+ */
 struct sp_icm_reg {
-	/*
-	u32 enable:1;
-	u32 reload:1; // reload setting
-	u32 intclr:1; // write 1 clear interrupt
-	u32 muxsel:3; // select input signal source
-	u32 clksel:3; // select clock source for counter
-	u32 rsv0:7;
-	u32 msk0:16;
-	*/
 	u32 cfg0;
-
-	/*
-	u32 eemode:2; // edge mode: 0 rising / 1 falling / 2 both
-	u32 etimes:4; // event times (0~15)
-	u32 dtimes:3; // debounce times (0~7)
-	u32 rsv1:3;
-	u32 fddrop:1; // fifo data drop
-	u32 fclear:1; // fifo clear
-	u32 fempty:1; // fifo empty
-	u32 ffull :1; // fifo full
-	u32 msk1:16;
-	*/
 	u32 cfg1;
 
-	u32 cntscl;	// counter clock prescaler: cnt_clk = ext_clk / (cnt_scl + 1)
-	u32 tstscl;	// test signal clock prescaler: tst_clk = sysclk / (tst_scl + 1)
-	u32 cnt;	// counter, read from fifo
+	u32 cntscl;
+	u32 tstscl;
+	u32 cnt;
 
-	u32 pwh;	// pulse width high
-	u32 pwl;	// pulse width low
+	u32 pwh;
+	u32 pwl;
 };
 
 struct sp_icm_dev {
-	volatile struct sp_icm_reg *reg;
+	struct sp_icm_reg *reg;
 	int irq;
 	struct clk *clk;
 	struct device *dev;
@@ -103,32 +120,30 @@ static sp_icm_cbf cbfs[NUM_ICM];
 static irqreturn_t sp_icm_isr(int irq, void *dev_id)
 {
 	int i = irq - sp_icm.irq;
-	volatile struct sp_icm_reg *icm = &sp_icm.reg[i];
+	struct sp_icm_reg *icm = &sp_icm.reg[i];
 	u32 cnt, fstate;
 	int clk_bk;
 	//TRACE("");
 	clk_bk = icm->cfg0 & 0x000001C0;
 	icm->cfg0 = 0x01C00100;
-	icm->cfg0 = ICM_INTCLR; // clear interrupt
+	icm->cfg0 = ICM_INTCLR; /* clear interrupt */
 	icm->cfg0 = 0x01C00000 | clk_bk;
 
-	while (!((fstate = icm->cfg1) & ICM_FEMPTY)) { // fifo not empty
-		cnt = icm->cnt; // read counter from fifo
-		if (cbfs[i]) {
-			(*cbfs[i])(i, cnt, fstate & ICM_FMASK); // callback
-		}
+	while (!((fstate = icm->cfg1) & ICM_FEMPTY)) { /* fifo not empty */
+		cnt = icm->cnt; /* read counter from fifo */
+		if (cbfs[i])
+			(*cbfs[i])(i, cnt, fstate & ICM_FMASK); /* callback */
 	}
 
 	return IRQ_HANDLED;
 }
 
-
-
 int sp_icm_setcfg(int i, struct sp_icm_cfg *cfg)
 {
-	volatile struct sp_icm_reg *icm;
+	struct sp_icm_reg *icm;
 
-	if (i < 0 || i >= NUM_ICM) return -EINVAL;
+	if (i < 0 || i >= NUM_ICM)
+		return -EINVAL;
 
 	RPM_GET(sp_icm.dev);
 	icm = &sp_icm.reg[i];
@@ -147,9 +162,10 @@ EXPORT_SYMBOL(sp_icm_setcfg);
 
 int sp_icm_getcfg(int i, struct sp_icm_cfg *cfg)
 {
-	volatile struct sp_icm_reg *icm;
+	struct sp_icm_reg *icm;
 
-	if (i < 0 || i >= NUM_ICM) return -EINVAL;
+	if (i < 0 || i >= NUM_ICM)
+		return -EINVAL;
 
 	RPM_GET(sp_icm.dev);
 	icm = &sp_icm.reg[i];
@@ -168,112 +184,141 @@ EXPORT_SYMBOL(sp_icm_getcfg);
 
 int sp_icm_reload(int i)
 {
-	if (i < 0 || i >= NUM_ICM) return -EINVAL;
+	if (i < 0 || i >= NUM_ICM)
+		return -EINVAL;
+
 	RPM_GET(sp_icm.dev);
 	sp_icm.reg[i].cfg0 = ICM_RELOAD;
 	RPM_PUT(sp_icm.dev);
+
 	return 0;
 }
 EXPORT_SYMBOL(sp_icm_reload);
 
 int sp_icm_enable(int i, sp_icm_cbf cbf)
 {
-	if (i < 0 || i >= NUM_ICM) return -EINVAL;
+	if (i < 0 || i >= NUM_ICM)
+		return -EINVAL;
+
 	cbfs[i] = cbf;
 	RPM_GET(sp_icm.dev);
 	sp_icm.reg[i].cfg0 = ICM_ENABLE;
+
 	return 0;
 }
 EXPORT_SYMBOL(sp_icm_enable);
 
 int sp_icm_disable(int i)
 {
-	if (i < 0 || i >= NUM_ICM) return -EINVAL;
+	if (i < 0 || i >= NUM_ICM)
+		return -EINVAL;
+
 	sp_icm.reg[i].cfg0 = ICM_DISABLE;
-	sp_icm.reg[i].cfg1 = ICM_FCLEAR; // clear fifo
+	sp_icm.reg[i].cfg1 = ICM_FCLEAR; /* clear fifo */
 	RPM_PUT(sp_icm.dev);
 	cbfs[i] = NULL;
+
 	return 0;
 }
 EXPORT_SYMBOL(sp_icm_disable);
 
 int sp_icm_fstate(int i, u32 *fstate)
 {
-	if (i < 0 || i >= NUM_ICM) return -EINVAL;
+	if (i < 0 || i >= NUM_ICM)
+		return -EINVAL;
+
 	RPM_GET(sp_icm.dev);
 	*fstate = sp_icm.reg[i].cfg1 & ICM_FMASK;
 	RPM_PUT(sp_icm.dev);
+
 	return 0;
 }
 EXPORT_SYMBOL(sp_icm_fstate);
 
 int sp_icm_pwidth(int i, u32 *pwh, u32 *pwl)
 {
-	if (i < 0 || i >= NUM_ICM) return -EINVAL;
+	if (i < 0 || i >= NUM_ICM)
+		return -EINVAL;
+
 	RPM_GET(sp_icm.dev);
 	*pwh = sp_icm.reg[i].pwh;
 	*pwl = sp_icm.reg[i].pwl;
 	RPM_PUT(sp_icm.dev);
+
 	return 0;
 }
 EXPORT_SYMBOL(sp_icm_pwidth);
 
-
-#ifdef CONFIG_SP_ICM_TEST // test & example
-static u32 tscnt = 0; // test signal counter
+#ifdef CONFIG_SP_ICM_TEST /* test & example */
+static u32 tscnt; /* test signal counter */
 
 static void test_cbf(int i, u32 cnt, u32 fstate)
 {
 	u32 pwh, pwl;
+
 	sp_icm_pwidth(i, &pwh, &pwl);
-	printk("icm%d_%05u: %10u %04x %u %u\n", i, ++tscnt, cnt, fstate, pwh, pwl);
+	pr_info("icm%d_%05u: %10u %04x %u %u\n", i, ++tscnt, cnt, fstate,
+			pwh, pwl);
 }
 
 static void test_help(void)
 {
-	printk(
+	pr_info(
 		"sp_icm test:\n"
-		"  echo <icm:0~3> [function] [params...] > /sys/module/sp_icm/parameters/test\n"
+		"  echo <icm:0~3> [function] [params...]\n"
+		" > /sys/module/sp_icm/parameters/test\n"
 		"  * if no function & params, dump icm cfg & state\n"
 		"  function:\n"
 		"    0: disable icm, no params\n"
 		"    1: enable icm\n"
 		"    2: reload icm\n"
 		"  params in order:\n"
-		"    muxsel, clksel, eemode, etimes, dtimes, cntscl, tstscl, tstime(ms)\n"
+		"    muxsel, clksel, eemode, etimes, dtimes,\n"
+		"    cntscl, tstscl, tstime(ms)\n"
 		"    * -1 means no change\n"
 	);
 }
 
 static int test_set(const char *val, const struct kernel_param *kp)
 {
-	int i, f, muxsel, clksel, eemode, etimes, dtimes, cntscl, tstscl, tstime;
+	int i, f, muxsel, clksel, eemode, etimes, dtimes;
+	int cntscl, tstscl, tstime;
 	u32 fstate, pwh, pwl;
 	struct sp_icm_cfg cfg;
+	int ret;
 
-	i = f = muxsel = clksel = eemode = etimes = dtimes = cntscl = tstscl = tstime = -1;
-	sscanf(val, "%d %d %d %d %d %d %d %d %d %d",
-		&i, &f, &muxsel, &clksel, &eemode, &etimes, &dtimes, &cntscl, &tstscl, &tstime);
+	i = f = muxsel = clksel = eemode = etimes = dtimes
+		= cntscl = tstscl = tstime = -1;
+	ret = sscanf(val, "%d %d %d %d %d %d %d %d %d %d",
+		&i, &f, &muxsel, &clksel, &eemode, &etimes,
+		&dtimes, &cntscl, &tstscl, &tstime);
 
 	if (i >= 0 && i < NUM_ICM) {
 		switch (f) {
-		case 0: // disable
+		case 0: /* disable */
 disable:
 			sp_icm_disable(i);
-			printk("icm%d: tscnt = %u\n", i, tscnt);
+			pr_info("icm%d: tscnt = %u\n", i, tscnt);
 			tscnt = 0;
 			return 0;
 
-		case 1: // enable
-		case 2: // reload
+		case 1: /* enable */
+		case 2: /* reload */
 			sp_icm_getcfg(i, &cfg);
-			if (muxsel != -1) cfg.muxsel = (u32)muxsel;
-			if (clksel != -1) cfg.clksel = (u32)clksel;
-			if (eemode != -1) cfg.eemode = (u32)eemode;
-			if (etimes != -1) cfg.etimes = (u32)etimes;
-			if (dtimes != -1) cfg.dtimes = (u32)dtimes;
-			if (cntscl != -1) cfg.cntscl = (u32)cntscl;
-			if (tstscl != -1) cfg.tstscl = (u32)tstscl;
+			if (muxsel != -1)
+				cfg.muxsel = (u32)muxsel;
+			if (clksel != -1)
+				cfg.clksel = (u32)clksel;
+			if (eemode != -1)
+				cfg.eemode = (u32)eemode;
+			if (etimes != -1)
+				cfg.etimes = (u32)etimes;
+			if (dtimes != -1)
+				cfg.dtimes = (u32)dtimes;
+			if (cntscl != -1)
+				cfg.cntscl = (u32)cntscl;
+			if (tstscl != -1)
+				cfg.tstscl = (u32)tstscl;
 			sp_icm_setcfg(i, &cfg);
 			if (f == 1) {
 				sp_icm_enable(i, test_cbf);
@@ -289,16 +334,16 @@ disable:
 			sp_icm_getcfg(i, &cfg);
 			sp_icm_fstate(i, &fstate);
 			sp_icm_pwidth(i, &pwh, &pwl);
-			printk("sp_icm%d cfg & state:", i);
-			printk("muxsel: %u", cfg.muxsel);
-			printk("clksel: %u", cfg.clksel);
-			printk("eemode: %u", cfg.eemode);
-			printk("etimes: %u", cfg.etimes);
-			printk("dtimes: %u", cfg.dtimes);
-			printk("cntscl: %u", cfg.cntscl);
-			printk("tstscl: %u", cfg.tstscl);
-			printk("fstate: %04x", fstate);
-			printk("pwidth: %u %u\n", pwh, pwl);
+			pr_info("sp_icm%d cfg & state:", i);
+			pr_info("muxsel: %u", cfg.muxsel);
+			pr_info("clksel: %u", cfg.clksel);
+			pr_info("eemode: %u", cfg.eemode);
+			pr_info("etimes: %u", cfg.etimes);
+			pr_info("dtimes: %u", cfg.dtimes);
+			pr_info("cntscl: %u", cfg.cntscl);
+			pr_info("tstscl: %u", cfg.tstscl);
+			pr_info("fstate: %04x", fstate);
+			pr_info("pwidth: %u %u\n", pwh, pwl);
 			return 0;
 		}
 	}
@@ -316,7 +361,7 @@ module_param_cb(test, &test_ops, NULL, 0600);
 
 static const struct of_device_id sp_icm_of_match[] = {
 	{ .compatible = "sunplus,sp7021-icm" },
-	{ /* sentinel */ }
+	{},
 };
 MODULE_DEVICE_TABLE(of, sp_icm_of_match);
 
@@ -333,9 +378,9 @@ static int sp_icm_probe(struct platform_device *pdev)
 		dev_err(&pdev->dev, "not found clk source\n");
 		return PTR_ERR(dev->clk);
 	}
+
 	ret = clk_prepare_enable(dev->clk);
-	//ret = clk_prepare(dev->clk);
-	if(ret)
+	if (ret)
 		return ret;
 
 	res_mem = platform_get_resource(pdev, IORESOURCE_MEM, 0);
@@ -347,9 +392,8 @@ static int sp_icm_probe(struct platform_device *pdev)
 		return PTR_ERR((void *)dev->reg);
 
 	res_irq = platform_get_resource(pdev, IORESOURCE_IRQ, 0);
-	if (!res_irq) {
+	if (!res_irq)
 		return -ENODEV;
-	}
 
 	dev->irq = res_irq->start;
 	platform_set_drvdata(pdev, dev);
@@ -361,10 +405,10 @@ static int sp_icm_probe(struct platform_device *pdev)
 	RPM_PUT(dev->dev);
 
 	while (i < NUM_ICM) {
-		ret = devm_request_irq(&pdev->dev, dev->irq + i, sp_icm_isr, IRQF_TRIGGER_RISING, "sp_icm", dev);
-		if (ret) {
+		ret = devm_request_irq(&pdev->dev, dev->irq + i, sp_icm_isr,
+			IRQF_TRIGGER_RISING, "sp_icm", dev);
+		if (ret)
 			return -ENODEV;
-		}
 		i++;
 	}
 
@@ -411,7 +455,7 @@ static int __maybe_unused sp_icm_runtime_resume(struct device *dev)
 
 static const struct dev_pm_ops sp_icm_pm_ops = {
 	SET_SYSTEM_SLEEP_PM_OPS(sp_icm_suspend, sp_icm_resume)
-	SET_RUNTIME_PM_OPS(sp_icm_runtime_suspend,sp_icm_runtime_resume, NULL)
+	SET_RUNTIME_PM_OPS(sp_icm_runtime_suspend, sp_icm_runtime_resume, NULL)
 };
 
 static struct platform_driver sp_icm_driver = {
--- include/misc/sp_icm.h
+++ include/misc/sp_icm.h
@@ -1,63 +1,81 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Sunplus SP7021 SoC ICM(Input Capture Module) driver header file
+ */
+
 #ifndef __SP_ICM_H__
 #define __SP_ICM_H__
 
-// muxsel: input signal source select
-#define ICM_MUXSEL_INPUT0	0 // input signal 0
-#define ICM_MUXSEL_INPUT1	1 // input signal 1
-#define ICM_MUXSEL_INPUT2	2 // input signal 2
-#define ICM_MUXSEL_INPUT3	3 // input signal 3
-#define ICM_MUXSEL_TEST		4 // test signal
+/*
+ * muxsel: input signal source select
+ */
+#define ICM_MUXSEL_INPUT0	0 /* input signal 0 */
+#define ICM_MUXSEL_INPUT1	1 /* input signal 1 */
+#define ICM_MUXSEL_INPUT2	2 /* input signal 2 */
+#define ICM_MUXSEL_INPUT3	3 /* input signal 3 */
+#define ICM_MUXSEL_TEST		4 /* test signal */
 
-// clksel: internal counter clock select
-#define ICM_CLKSEL_EXT0		0 // external clock 0
-#define ICM_CLKSEL_EXT1		1 // external clock 1
-#define ICM_CLKSEL_EXT2		2 // external clock 2
-#define ICM_CLKSEL_EXT3		3 // external clock 3
-#define ICM_CLKSEL_SYS		4 // system clock
-#define ICM_CLKSEL_27M		5 // 27 MHz
-#define ICM_CLKSEL_32K		6 // 32 KHz
-	
-// eemode: edge event mode
-#define ICM_EEMODE_RISING	0 // rising edge
-#define ICM_EEMODE_FALLING	1 // falling edge
-#define ICM_EEMODE_BOTH		2 // both edges
+/*
+ * clksel: internal counter clock select
+ */
+#define ICM_CLKSEL_EXT0		0 /* external clock 0 */
+#define ICM_CLKSEL_EXT1		1 /* external clock 1 */
+#define ICM_CLKSEL_EXT2		2 /* external clock 2 */
+#define ICM_CLKSEL_EXT3		3 /* external clock 3 */
+#define ICM_CLKSEL_SYS		4 /* system clock */
+#define ICM_CLKSEL_27M		5 /* 27 MHz */
+#define ICM_CLKSEL_32K		6 /* 32 kHz */
 
-// etimes: edge event times
-// set 0 ~ 15. if set 7, interrupt will be triggered after
-// 8 times edge event.
+/*
+ * eemode: edge event mode
+ */
+#define ICM_EEMODE_RISING	0 /* rising edge */
+#define ICM_EEMODE_FALLING	1 /* falling edge */
+#define ICM_EEMODE_BOTH		2 /* both edges */
 
-// dtimes: debounce times of input signal debounce filter
-// 0~6: (dtimes + 1) times
-// 7  : 16 times
-
-// cntscl: internal counter clock prescaler: 0 ~ (2^32-1)
-// cnt_clk = clksel_clk / (cntscl + 1)
-// internal counter +1 @ cnt_clk rising edge
-
-// tstscl: test signal clock prescaler: 0 ~ (2^32-1)
-// tst_clk = sysclk / (tstscl + 1)
-
+/*
+ * etimes: edge event times
+ * set 0 ~ 15. if set 7, interrupt will be triggered after
+ * 8 times edge event.
+ *
+ * dtimes: debounce times of input signal debounce filter
+ * 0~6: (dtimes + 1) times
+ * 7  : 16 times
+ *
+ * cntscl: internal counter clock prescaler: 0 ~ (2^32-1)
+ * cnt_clk = clksel_clk / (cntscl + 1)
+ * internal counter +1 @ cnt_clk rising edge
+ *
+ * tstscl: test signal clock prescaler: 0 ~ (2^32-1)
+ * tst_clk = sysclk / (tstscl + 1)
+ */
 struct sp_icm_cfg {
-	u32 muxsel;
-	u32 clksel;
-	u32 eemode;
-	u32 etimes;
-	u32 dtimes;
-	u32 cntscl;
-	u32 tstscl;
+	u32 muxsel; /* input signal source select */
+	u32 clksel; /* internal counter clock select */
+	u32 eemode; /* edge event mode */
+	u32 etimes; /* edge event times */
+	u32 dtimes; /* debounce times of input signal debounce filter */
+	u32 cntscl; /* internal counter clock prescaler */
+	u32 tstscl; /* test signal clock prescaler */
 };
 
-// icm fifo state
-#define ICM_FDDROP	0x1000 // fifo data dropped
-#define ICM_FEMPTY	0x4000 // fifo empty
-#define ICM_FFULL	0x8000 // fifo full
+/*
+ * icm fifo state
+ */
+#define ICM_FDDROP	0x1000 /* fifo data dropped */
+#define ICM_FEMPTY	0x4000 /* fifo empty */
+#define ICM_FFULL	0x8000 /* fifo full */
 
-// icm callback function
-// cnt   : internal counter
-// fstate: fifo state
+/*
+ * icm callback function
+ * cnt   : internal counter
+ * fstate: fifo state
+ */
 typedef void (*sp_icm_cbf)(int icm, u32 cnt, u32 fstate);
 
-// icm: 0 ~ 3 (icm0 ~ icm3)
+/*
+ * icm: 0 ~ 3 (icm0 ~ icm3)
+ */
 extern int sp_icm_config(int icm, struct sp_icm_cfg *cfg);
 extern int sp_icm_reload(int icm);
 extern int sp_icm_enable(int icm, sp_icm_cbf cbf);
