602d42403fa734cca0dc542f5eb661857b30e7bc
vincent.shih <vincent.shih@sunplus.com>
2021-09-14 16:07:57 +0800

    [drivers][otp] Modified the driver to for linux                coding style.

--- include/linux/firmware/sp-ocotp.h
+++ include/linux/firmware/sp-ocotp.h
@@ -2,90 +2,128 @@
 
 #ifndef __SP_OCOTP_FW_H_
 #define __SP_OCOTP_FW_H_
-/*
- * OTP memory
- * Each bank contains 4 words (32 bits).
- * Bank 0 starts at offset 0 from the base.
- *
- */
+
+/**************************************************************************************************/
+/* OTP memory											  */
+/* Each bank contains 4 words (32 bits).							  */
+/* Bank 0 starts at offset 0 from the base.							  */
+/*												  */
+/**************************************************************************************************/
 #define OTP_WORDS_PER_BANK		4
 #define OTP_WORD_SIZE			sizeof(u32)
-#define OTP_BIT_ADDR_OF_BANK		(8 * OTP_WORD_SIZE * \
-						OTP_WORDS_PER_BANK)
+#define OTP_BIT_ADDR_OF_BANK		(8 * OTP_WORD_SIZE * OTP_WORDS_PER_BANK)
 
 #define QAC628_OTP_NUM_BANKS            8
-#define QAC628_OTP_SIZE                 (QAC628_OTP_NUM_BANKS * \
-						OTP_WORDS_PER_BANK * \
-						OTP_WORD_SIZE)
+#define QAC628_OTP_SIZE                 (QAC628_OTP_NUM_BANKS * OTP_WORDS_PER_BANK * OTP_WORD_SIZE)
 
 #define I143_OTP_NUM_BANKS		8
-#define I143_OTP_SIZE			(I143_OTP_NUM_BANKS * \
-						OTP_WORDS_PER_BANK * \
-						OTP_WORD_SIZE)
+#define I143_OTP_SIZE			(I143_OTP_NUM_BANKS * OTP_WORDS_PER_BANK * OTP_WORD_SIZE)
 
 #define QAK645_EFUSE0_NUM_BANKS		8
-#define QAK645_EFUSE0_SIZE		(QAK645_EFUSE0_NUM_BANKS * \
-						OTP_WORDS_PER_BANK * \
-						OTP_WORD_SIZE)
+#define QAK645_EFUSE0_SIZE		(QAK645_EFUSE0_NUM_BANKS * OTP_WORDS_PER_BANK * \
+										OTP_WORD_SIZE)
 
 #define QAK645_EFUSE1_NUM_BANKS		64
-#define QAK645_EFUSE1_SIZE		(QAK645_EFUSE1_NUM_BANKS * \
-						OTP_WORDS_PER_BANK * \
-						OTP_WORD_SIZE)
+#define QAK645_EFUSE1_SIZE		(QAK645_EFUSE1_NUM_BANKS * OTP_WORDS_PER_BANK * \
+										OTP_WORD_SIZE)
 
 #define QAK645_EFUSE2_NUM_BANKS		8
-#define QAK645_EFUSE2_SIZE		(QAK645_EFUSE2_NUM_BANKS * \
-						OTP_WORDS_PER_BANK * \
-						OTP_WORD_SIZE)
+#define QAK645_EFUSE2_SIZE		(QAK645_EFUSE2_NUM_BANKS * OTP_WORDS_PER_BANK * \
+										OTP_WORD_SIZE)
 
 #define OTP_READ_TIMEOUT                20000
 
 /* HB_GPIO register map */
-#define ADDRESS_8_DATA                  0x20
-#define ADDRESS_9_DATA                  0x24
-#define ADDRESS_10_DATA                 0x28
-#define ADDRESS_11_DATA                 0x2C
+struct sp_hb_gpio_reg {
+	unsigned int hb_gpio_rgst_bus32_1;
+	unsigned int hb_gpio_rgst_bus32_2;
+	unsigned int hb_gpio_rgst_bus32_3;
+	unsigned int hb_gpio_rgst_bus32_4;
+	unsigned int hb_gpio_rgst_bus32_5;
+	unsigned int hb_gpio_rgst_bus32_6;
+	unsigned int hb_gpio_rgst_bus32_7;
+	unsigned int hb_gpio_rgst_bus32_8;
+	unsigned int hb_gpio_rgst_bus32_9;
+	unsigned int hb_gpio_rgst_bus32_10;
+	unsigned int hb_gpio_rgst_bus32_11;
+	unsigned int hb_gpio_rgst_bus32_12;
+};
 
 /* OTPRX register map */
-#define OTP_PROGRAM_CONTROL             0x0C
+struct sp_otprx_reg {
+	unsigned int sw_trim;
+	unsigned int set_key;
+	unsigned int otp_rsv;
+	unsigned int otp_prog_ctl;
+	unsigned int otp_prog_addr;
+	unsigned int otp_prog_csv;
+	unsigned int otp_prog_strobe;
+	unsigned int otp_prog_load;
+	unsigned int otp_prog_pgenb;
+	unsigned int otp_prog_wr;
+	unsigned int otp_prog_reg25;
+	unsigned int otp_prog_state;
+	unsigned int otp_usb_phy_trim;
+	unsigned int otp_data2;
+	unsigned int otp_prog_ps;
+	unsigned int otp_rsv2;
+	unsigned int key_srst;
+	unsigned int otp_ctrl;
+	unsigned int otp_cmd;
+	unsigned int otp_cmd_status;
+	unsigned int otp_addr;
+	unsigned int otp_data;
+	unsigned int rtc_reset_record;
+	unsigned int rtc_battery_ctrl;
+	unsigned int rtc_trim_ctrl;
+	unsigned int rsv25;
+	unsigned int rsv26;
+	unsigned int rsv27;
+	unsigned int rsv28;
+	unsigned int rsv29;
+	unsigned int rsv30;
+	unsigned int rsv31;
+};
+
+// otp_prog_ctl
 #define PIO_MODE                        0x07
 
-#define OTP_PROGRAM_ADDRESS             0x10
+// otp_prog_addr
 #define PROG_OTP_ADDR                   0x1FFF
 
-#define OTP_PROGRAM_PGENB               0x20
+// otp_prog_pgenb
 #define PIO_PGENB                       0x01
 
-#define OTP_PROGRAM_ENABLE              0x24
+// otp_prog_wr
 #define PIO_WR                          0x01
 
-#define OTP_PROGRAM_VDD2P5              0x28
+// otp_prog_reg25
 #define PROGRAM_OTP_DATA                0xFF00
 #define PROGRAM_OTP_DATA_SHIFT          8
 #define REG25_PD_MODE_SEL               0x10
 #define REG25_POWER_SOURCE_SEL          0x02
 #define OTP2REG_PD_N_P                  0x01
 
-#define OTP_PROGRAM_STATE               0x2C
+// otp_prog_state
 #define OTPRSV_CMD3                     0xE0
 #define OTPRSV_CMD3_SHIFT               5
 #define TSMC_OTP_STATE                  0x1F
 
-#define OTP_CONTROL                     0x44
+// otp_ctrl
 #define PROG_OTP_PERIOD                 0xFFE0
 #define PROG_OTP_PERIOD_SHIFT           5
 #define OTP_EN_R                        0x01
 
-#define OTP_CONTROL2                    0x48
+// otp_cmd
 #define OTP_RD_PERIOD                   0xFF00
 #define OTP_RD_PERIOD_SHIFT             8
 #define OTP_READ                        0x04
 
-#define OTP_STATUS                      0x4C
+// otp_cmd_status
 #define OTP_READ_DONE                   0x10
 
-#define OTP_READ_ADDRESS                0x50
-#define RD_OTP_ADDRESS                  0x1F
+// otp_addr
+#define RD_OTP_ADDRESS                  0x1FFF
 
 extern int sp_ocotp_probe(struct platform_device *pdev);
 extern int sp_ocotp_remove(struct platform_device *pdev);
