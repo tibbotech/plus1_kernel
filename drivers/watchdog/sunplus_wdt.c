f1cae9873c37aac276b406c6af9716a9534ee79d
Xiantao Hu <xt.hu@cqplus1.com>
2021-11-24 18:54:44 +0800

    [drivers][watchdog] Modify code to v2.

--- drivers/watchdog/sunplus_wdt.c
+++ drivers/watchdog/sunplus_wdt.c
@@ -29,8 +29,8 @@
 
 #define MASK_SET(mask)		((mask) | (mask << 16))
 
-#define SP_WDT_MAX_TIMEOUT		11
-#define SP_WDT_MIN_TIMEOUT		1
+#define SP_WDT_MAX_TIMEOUT		11U
+#define SP_WDT_DEFAULT_TIMEOUT	10
 
 #define STC_CLK				90000
 
@@ -48,7 +48,6 @@ MODULE_PARM_DESC(nowayout, "Watchdog cannot be stopped once started (default="
 struct sp_wdt_priv {
 	struct watchdog_device wdev;
 	void __iomem *base;
-	void __iomem *miscellaneous;
 	struct clk *clk;
 	struct reset_control *rstc;
 };
@@ -57,12 +56,12 @@ static int sp_wdt_restart(struct watchdog_device *wdev,
 			       unsigned long action, void *data)
 {
 	struct sp_wdt_priv *priv = watchdog_get_drvdata(wdev);
-	void __iomem *base;
+	void __iomem *base = priv->base;
 
-	base = priv->base;
 	writel(WDT_STOP, base + WDT_CTRL);
 	writel(WDT_UNLOCK, base + WDT_CTRL);
 	writel(0x0001, base + WDT_CNT);
+	writel(WDT_LOCK, base + WDT_CTRL);
 	writel(WDT_RESUME, base + WDT_CTRL);
 
 	return 0;
@@ -74,13 +73,19 @@ static int sp_wdt_ping(struct watchdog_device *wdev)
 	struct sp_wdt_priv *priv = watchdog_get_drvdata(wdev);
 	void __iomem *base = priv->base;
 	u32 count;
+	u32 actual;
 
-	writel(WDT_STOP, base + WDT_CTRL);
-	writel(WDT_UNLOCK, base + WDT_CTRL);
-	/* tiemrw_cnt[3:0]cant be write,only [19:4] can be write. */
-	count = (wdev->timeout * STC_CLK) >> 4;
-	writel(count, base + WDT_CNT);
-	writel(WDT_RESUME, base + WDT_CTRL);
+	actual = min(wdev->timeout, SP_WDT_MAX_TIMEOUT);
+
+	if (actual > SP_WDT_MAX_TIMEOUT) {
+		writel(WDT_CONMAX, base + WDT_CTRL);
+	} else {
+		writel(WDT_UNLOCK, base + WDT_CTRL);
+		/* tiemrw_cnt[3:0]can't be write,only [19:4] can be write. */
+		count = (actual * STC_CLK) >> 4;
+		writel(count, base + WDT_CNT);
+		writel(WDT_LOCK, base + WDT_CTRL);
+	}
 
 	return 0;
 }
@@ -88,7 +93,9 @@ static int sp_wdt_ping(struct watchdog_device *wdev)
 static int sp_wdt_set_timeout(struct watchdog_device *wdev,
 				   unsigned int timeout)
 {
+
 	wdev->timeout = timeout;
+	sp_wdt_ping(wdev);
 
 	return 0;
 }
@@ -105,13 +112,10 @@ static int sp_wdt_stop(struct watchdog_device *wdev)
 
 static int sp_wdt_start(struct watchdog_device *wdev)
 {
-	int ret;
+	struct sp_wdt_priv *priv = watchdog_get_drvdata(wdev);
+	void __iomem *base = priv->base;
 
-	ret = sp_wdt_set_timeout(wdev, wdev->timeout);
-	if (ret < 0)
-		return ret;
-
-	sp_wdt_ping(wdev);
+	writel(WDT_RESUME, base + WDT_CTRL);
 
 	return 0;
 }
@@ -132,51 +136,64 @@ static unsigned int sp_wdt_get_timeleft(struct watchdog_device *wdev)
 /*
  * 1.We need to reset watchdog flag(clear watchdog interrupt) here
  * because watchdog timer driver does not have an interrupt handler,
- * and before enalbe STC and RBUS watchdog timeout. Otherwise,
+ * and before enable STC and RBUS watchdog timeout. Otherwise,
  * the intr is always in the triggered state.
- * 2.enable STC and RBUS watchdog timeout trigger.
- * 3.watchdog conuter is running, need to be stopped.
+ * 2.Enable STC and RBUS watchdog timeout trigger.
+ * 3.Watchdog counter is running, need to be stopped.
  */
-static int sp_wdt_hw_init(struct watchdog_device *wdev)
+static int sp_wdt_hw_init(struct watchdog_device *wdev, void __iomem *rst_en)
 {
 	struct sp_wdt_priv *priv = watchdog_get_drvdata(wdev);
 	void __iomem *base = priv->base;
-	void __iomem *miscellaneous = priv->miscellaneous;
 	u32 val;
 
 	writel(WDT_CLRIRQ, base + WDT_CTRL);
-	val = readl(miscellaneous);
+	val = readl(rst_en);
 	val |= MASK_SET(STC_WDT_RST);
 	val |= MASK_SET(RBUS_WDT_RST);
-	writel(val, miscellaneous);
+	writel(val, rst_en);
 
 	sp_wdt_stop(wdev);
+	writel(WDT_LOCK, base + WDT_CTRL);
+	writel(WDT_CONMAX, base + WDT_CTRL);
 
 	return 0;
 }
 
 static const struct watchdog_info sp_wdt_info = {
-	.identity = DEVICE_NAME,
-	.options = WDIOF_SETTIMEOUT | WDIOF_KEEPALIVEPING | WDIOF_MAGICCLOSE,
+	.identity	= DEVICE_NAME,
+	.options	= WDIOF_SETTIMEOUT |
+			  WDIOF_MAGICCLOSE |
+			  WDIOF_KEEPALIVEPING,
 };
 
 static const struct watchdog_ops sp_wdt_ops = {
-	.owner = THIS_MODULE,
-	.start = sp_wdt_start,
-	.stop = sp_wdt_stop,
-	.ping = sp_wdt_ping,
-	.set_timeout = sp_wdt_set_timeout,
-	.get_timeleft = sp_wdt_get_timeleft,
-	.restart = sp_wdt_restart,
+	.owner		= THIS_MODULE,
+	.start		= sp_wdt_start,
+	.stop		= sp_wdt_stop,
+	.ping		= sp_wdt_ping,
+	.set_timeout	= sp_wdt_set_timeout,
+	.get_timeleft	= sp_wdt_get_timeleft,
+	.restart	= sp_wdt_restart,
 };
+
+static void sp_clk_disable_unprepare(void *data)
+{
+	clk_disable_unprepare(data);
+}
+
+static void sp_reset_control_assert(void *data)
+{
+	reset_control_assert(data);
+}
 
 static int sp_wdt_probe(struct platform_device *pdev)
 {
 	struct device *dev = &pdev->dev;
 	struct sp_wdt_priv *priv;
 	struct resource *wdt_res;
-	struct clk *clk;
 	int err;
+	void __iomem *rst_en;
 
 	priv = devm_kzalloc(dev, sizeof(*priv), GFP_KERNEL);
 	if (!priv)
@@ -188,60 +205,62 @@ static int sp_wdt_probe(struct platform_device *pdev)
 		return PTR_ERR(priv->clk);
 	}
 
-	err = clk_prepare_enable(clk);
+	err = clk_prepare_enable(priv->clk);
 	if (err) {
 		dev_err(dev, "Clock can't be enabled correctly\n");
 		return err;
 	}
 
-	priv->rstc = devm_reset_control_get_exclusive(dev, NULL);
+	/* The timer and watchdog shared the STC reset */
+	priv->rstc = devm_reset_control_get_shared(dev, NULL);
 	if (!IS_ERR(priv->rstc))
 		reset_control_deassert(priv->rstc);
 
-	platform_set_drvdata(pdev, priv);
+	err = devm_add_action_or_reset(dev, sp_reset_control_assert,
+				       priv->rstc);
+	if (err)
+		return err;
+
+	err = devm_add_action_or_reset(dev, sp_clk_disable_unprepare,
+				       priv->clk);
+	if (err)
+		return err;
 
 	priv->base = devm_platform_ioremap_resource(pdev, 0);
 	if (IS_ERR(priv->base))
 		return PTR_ERR(priv->base);
 
+	/* The registers accessed here shared by multiple drivers. */
 	wdt_res = platform_get_resource(pdev, IORESOURCE_MEM, 1);
-	priv->miscellaneous =
+	rst_en =
 	    devm_ioremap(dev, wdt_res->start, resource_size(wdt_res));
-	if (IS_ERR(priv->miscellaneous))
-		return PTR_ERR(priv->miscellaneous);
+	if (IS_ERR(rst_en))
+		return PTR_ERR(rst_en);
 
 	priv->wdev.info = &sp_wdt_info;
 	priv->wdev.ops = &sp_wdt_ops;
-	priv->wdev.timeout = SP_WDT_MAX_TIMEOUT;
-	priv->wdev.max_timeout = SP_WDT_MAX_TIMEOUT;
-	priv->wdev.min_timeout = SP_WDT_MIN_TIMEOUT;
+	priv->wdev.timeout = SP_WDT_DEFAULT_TIMEOUT;
+	priv->wdev.max_hw_heartbeat_ms = SP_WDT_MAX_TIMEOUT * 1000;
+	priv->wdev.min_timeout = 1;
 	priv->wdev.parent = dev;
-
-	watchdog_set_drvdata(&priv->wdev, priv);
-	sp_wdt_hw_init(&priv->wdev);
 
 	watchdog_init_timeout(&priv->wdev, timeout, dev);
 	watchdog_set_nowayout(&priv->wdev, nowayout);
-	watchdog_stop_on_reboot(&priv->wdev);
+	watchdog_set_restart_priority(&priv->wdev, 128);
 
+	watchdog_set_drvdata(&priv->wdev, priv);
+
+	sp_wdt_hw_init(&priv->wdev, rst_en);
+
+	watchdog_stop_on_reboot(&priv->wdev);
 	err = devm_watchdog_register_device(dev, &priv->wdev);
-	if (unlikely(err))
+	if (err)
 		return err;
+
+	platform_set_drvdata(pdev, priv);
 
 	dev_info(dev, "Watchdog enabled (timeout=%d sec%s.)\n",
 		 priv->wdev.timeout, nowayout ? ", nowayout" : "");
-
-	return 0;
-}
-
-static int sp_wdt_remove(struct platform_device *pdev)
-{
-	struct sp_wdt_priv *priv = platform_get_drvdata(pdev);
-
-	watchdog_unregister_device(&priv->wdev);
-
-	reset_control_assert(priv->rstc);
-	clk_disable_unprepare(priv->clk);
 
 	return 0;
 }
@@ -267,13 +286,20 @@ static int __maybe_unused sp_wdt_suspend(struct device *dev)
 
 static int __maybe_unused sp_wdt_resume(struct device *dev)
 {
+	int err;
+
 	struct sp_wdt_priv *priv = dev_get_drvdata(dev);
+
+	err = clk_prepare_enable(priv->clk);
+	if (err) {
+		dev_err(dev, "Clock can't be enabled correctly\n");
+		return err;
+	}
+
+	reset_control_deassert(priv->rstc);
 
 	if (watchdog_active(&priv->wdev))
 		sp_wdt_start(&priv->wdev);
-
-	reset_control_deassert(priv->rstc);
-	clk_prepare_enable(priv->clk);
 
 	return 0;
 }
@@ -282,7 +308,6 @@ static SIMPLE_DEV_PM_OPS(sp_wdt_pm_ops, sp_wdt_suspend, sp_wdt_resume);
 
 static struct platform_driver sp_wdt_driver = {
 	.probe = sp_wdt_probe,
-	.remove = sp_wdt_remove,
 	.driver = {
 		   .name = DEVICE_NAME,
 		   .of_match_table = sp_wdt_of_match,
