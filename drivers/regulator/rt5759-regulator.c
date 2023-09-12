0b4ebc08f4a009da38f3d1f414b2724bf4ca688a
Qin Jian <qinjian@sunmedia.com.cn>
2023-09-12 17:55:12 +0800

    [SP7350][cpufreq] update kernel 6.4 driver, no need sp7350 fix

--- drivers/regulator/rt5759-regulator.c
+++ drivers/regulator/rt5759-regulator.c
@@ -294,13 +294,11 @@ static int rt5759_regulator_register(struct rt5759_priv *priv)
 	if (priv->chip_type == CHIP_TYPE_RT5759A)
 		reg_desc->uV_step = RT5759A_STEP_UV;
 
+	memset(&reg_cfg, 0, sizeof(reg_cfg));
 	reg_cfg.dev = priv->dev;
 	reg_cfg.of_node = np;
 	reg_cfg.init_data = of_get_regulator_init_data(priv->dev, np, reg_desc);
 	reg_cfg.regmap = priv->regmap;
-#ifdef CONFIG_SOC_SP7350
-	reg_cfg.ena_gpiod = false;
-#endif
 
 	rdev = devm_regulator_register(priv->dev, reg_desc, &reg_cfg);
 	if (IS_ERR(rdev)) {
@@ -412,6 +410,7 @@ MODULE_DEVICE_TABLE(of, rt5759_device_table);
 static struct i2c_driver rt5759_driver = {
 	.driver = {
 		.name = "rt5759",
+		.probe_type = PROBE_PREFER_ASYNCHRONOUS,
 		.of_match_table = of_match_ptr(rt5759_device_table),
 	},
 	.probe_new = rt5759_probe,
