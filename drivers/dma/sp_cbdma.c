9133f2cb8d4770cfdc16ea51fa94e5e731725c82
Cheng Chung Ho <cc.ho@sunplus.com>
2023-12-13 15:21:28 +0800

    [drivers][dma] Modify CB-DMA driver to fix a compiler warning.

--- drivers/dma/sp_cbdma.c
+++ drivers/dma/sp_cbdma.c
@@ -2141,7 +2141,7 @@ static int sp_cbdma_remove(struct platform_device *pdev)
 
 static int sp_cbdma_pm_suspend(struct device *dev)
 {
-	struct sp_dma_device *xdev = dev_get_drvdata(dev);
+	//struct sp_dma_device *xdev = dev_get_drvdata(dev);
 	//int err;
 
 	/*
